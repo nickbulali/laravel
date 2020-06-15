919374cef37067ba227675f8d44ddd696d131d1a
Nick Bulali <nickbulali@yahoo.com>
2020-06-15 10:55:40 +0300

    'base'

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->group(function (){
+	Route::get('/user', function (Request $request) {
+    return $request->user();
+	});
+	Route::get('/todos', 'TodosController@index');
+	Route::post('/logout', 'AuthController@logout');
+	Route::post('/todos', 'TodosController@store');
+	Route::patch('/todos/{todo}', 'TodosController@update');
+	Route::delete('/todos/{todo}', 'TodosController@destroy');
+	Route::patch('/todosCheckAll', 'TodosController@updateAll');
+	Route::delete('/todosDelete', 'TodosController@destroyCompleted');
+
+
+
+
+});
+
+
+Route::post('/login', 'AuthController@login');
+Route::post('/resister', 'AuthController@register');
+
+
+
+
+
